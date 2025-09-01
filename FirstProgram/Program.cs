bfc732dbd4ed46cf45b288f8694f6846a3136d16
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-01 17:51:15 +0700

    cons

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,19 +1,3 @@
-﻿// variable
-
-int x; // declaration
-x = 10; // initialization
-
-Console.WriteLine(x);
-Console.WriteLine(x + 10);
-
-double age = 18.5; // demical number
-Console.WriteLine("Your age is " + age);
-
-bool beauty = true; // true or false
-Console.WriteLine("are you beauty? " + beauty);
-
-char character = 'A'; // single character
-Console.WriteLine("character: " + character);
-
-string name = "Minh"; // sequence of characters
-Console.WriteLine("my name is " + name);
+﻿// constants
+const double PI = 3.14159;
+Console.WriteLine("Pi = " + PI);
