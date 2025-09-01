8bbd796cf22ccdc8598fc5bfb61a35df1bccdf19
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-01 18:08:17 +0700

    type casting Convert to

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,3 +1,16 @@
-﻿// constants
-const double PI = 3.14159;
-Console.WriteLine("Pi = " + PI);
+﻿// type casting
+string a = "1";
+int b = Convert.ToInt32(a);
+double c = Convert.ToInt32(a);
+char d = Convert.ToChar(a); 
+string s = "true";
+bool e = Convert.ToBoolean(s);
+string f = Convert.ToString(a);
+
+Console.WriteLine(b.GetType());
+Console.WriteLine(c.GetType());
+Console.WriteLine(d.GetType());
+Console.WriteLine(e.GetType());
+Console.WriteLine(f.GetType());
+
+
