923119326d95db363ccdc3b8acbd66382389c1cd
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-02 20:48:29 +0700

    switch

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,16 +1,46 @@
-﻿// if statements
-int a;
-Console.WriteLine("Enter a number:");
-a = Convert.ToInt32(Console.ReadLine());
-if (a > 5)
+﻿// switches
+string day = "Monday";
+string activity;
+// c# under 8.0 switch expression
+switch (day)
 {
-    Console.WriteLine("a is greater than 5");
+    case "Monday":
+        activity = "Go to the gym";
+        break;
+    case "Tuesday":
+        activity = "Attend a cooking class";
+        break;
+    case "Wednesday":
+        activity = "Work on a personal project";
+        break;
+    case "Thursday":
+        activity = "Meet friends for dinner";
+        break;
+    case "Friday":
+        activity = "Watch a movie";
+        break;
+    case "Saturday":
+        activity = "Go hiking";
+        break;
+    case "Sunday":
+        activity = "Relax and read a book";
+        break;
+    default:
+        activity = "Invalid day";
+        break;
 }
-else if (a == 5)
+Console.WriteLine(activity);
+
+// c# 8.0 switch expression
+activity = day switch
 {
-    Console.WriteLine("a is equal to 5");
-}
-else
-{
-    Console.WriteLine("a is less than 5");
-}
+    "Monday" => "Go to the gym",
+    "Tuesday" => "Attend a cooking class",
+    "Wednesday" => "Work on a personal project",
+    "Thursday" => "Meet friends for dinner",
+    "Friday" => "Watch a movie",
+    "Saturday" => "Go hiking",
+    "Sunday" => "Relax and read a book",
+    _ => "Invalid day"
+};
+Console.WriteLine(activity);
