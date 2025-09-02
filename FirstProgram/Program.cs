8dda02f27c1167cf2f45e4b4b07a46e0bd7bd4fc
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-02 20:59:33 +0700

    while

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,46 +1,7 @@
-﻿// switches
-string day = "Monday";
-string activity;
-// c# under 8.0 switch expression
-switch (day)
+﻿// while loop
+string name = "";
+while (name == "")
 {
-    case "Monday":
-        activity = "Go to the gym";
-        break;
-    case "Tuesday":
-        activity = "Attend a cooking class";
-        break;
-    case "Wednesday":
-        activity = "Work on a personal project";
-        break;
-    case "Thursday":
-        activity = "Meet friends for dinner";
-        break;
-    case "Friday":
-        activity = "Watch a movie";
-        break;
-    case "Saturday":
-        activity = "Go hiking";
-        break;
-    case "Sunday":
-        activity = "Relax and read a book";
-        break;
-    default:
-        activity = "Invalid day";
-        break;
+    name = Console.ReadLine();
+    Console.WriteLine(name != "" ? "your name is " + name : "Please enter a valid name:");
 }
-Console.WriteLine(activity);
-
-// c# 8.0 switch expression
-activity = day switch
-{
-    "Monday" => "Go to the gym",
-    "Tuesday" => "Attend a cooking class",
-    "Wednesday" => "Work on a personal project",
-    "Thursday" => "Meet friends for dinner",
-    "Friday" => "Watch a movie",
-    "Saturday" => "Go hiking",
-    "Sunday" => "Relax and read a book",
-    _ => "Invalid day"
-};
-Console.WriteLine(activity);
