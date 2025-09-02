b583814e95deb4f08c7a4ecfe304c0f19b83886b
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-02 19:03:23 +0700

    math class

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,9 +1,38 @@
-﻿// user input
+﻿// math class 
 
-Console.WriteLine("what is your name?");
-string name = Console.ReadLine();
-Console.WriteLine("Hello, " + name + "!");
+double a = 3.99;
+int b = 5;
 
-Console.WriteLine("How old are you?");
-int age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("You are " + age + " years old.");
+double c = Math.Pow(a, b);
+double d = Math.Sqrt(a);
+double e = Math.Round(a);
+double f = Math.Ceiling(a);
+double g = Math.Floor(a);
+double h = Math.Abs(-a);
+double i = Math.Max(a, b);
+double j = Math.Min(a, b);
+double k = Math.PI;
+double l = Math.E;
+double m = Math.Log(a);
+double n = Math.Log10(a);
+double o = Math.Exp(a);
+double p = Math.Sin(a);
+double q = Math.Cos(a);
+double r = Math.Tan(a);
+
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
+Console.WriteLine(o);
+Console.WriteLine(p);
+Console.WriteLine(q);
+Console.WriteLine(r);
