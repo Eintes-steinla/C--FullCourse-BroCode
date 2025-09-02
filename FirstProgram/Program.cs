072a2291d175e4b71ba4d3df11fb135262c177f4
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-02 19:27:14 +0700

    string methods

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,3 +1,22 @@
-﻿// random number
-int a = new Random().Next(1, 100);
-Console.WriteLine($"Hello, World! Here is a random number: {a}");
+﻿// string method
+
+string fullName = "Nguyen Bao Minh";
+Console.WriteLine(fullName);
+
+fullName = fullName.ToUpper();
+Console.WriteLine(fullName);
+
+fullName = fullName.ToLower();
+Console.WriteLine(fullName);
+
+string phoneNumber = "0123-456-789";
+phoneNumber = phoneNumber.Replace("-", "");
+Console.WriteLine(phoneNumber);
+
+fullName = fullName.Insert(0, "Mr. ");
+Console.WriteLine(fullName);
+
+Console.WriteLine(fullName.Length);
+
+string firstName = fullName.Substring(4, 6);
+Console.WriteLine(firstName);
